b41744fa82dfcfe2af535d75a455a99da9fcaa03
steffen backes <backes@pmc04.cpht.polytechnique.fr>
2018-09-06 09:46:49 +0200

    product basis works!

--- prodbasis/main.f90
+++ prodbasis/main.f90
@@ -10,18 +10,18 @@ module constants
  
    integer,parameter  :: norb   = 3
    integer,parameter  :: nprodstates = 6
-   integer,parameter  :: nr     = 15
-   integer,parameter  :: ntheta = 15
-   integer,parameter  :: nphi   = 15
-   real(kr),parameter :: rmax   = 30.0_kr
+   integer,parameter  :: nr     = 30
+   integer,parameter  :: ntheta = 25
+   integer,parameter  :: nphi   = 25
+   real(kr),parameter :: rmax   = 1.5_kr
    real(kr),parameter :: dr     = rmax/nr
    real(kr),parameter :: dtheta = pi/ntheta
    real(kr),parameter :: dphi   = 2*pi/nphi
    real(kr),parameter :: hartree = 27.21138602 !eV
 
    real(kr),parameter :: beta   = 40.0_kr
-   real(kr),parameter :: Z = 1.0_kr
-   real(kr),parameter :: LagPref = sqrt( 1.0/720.0 ) * ( 2*Z/3.0_kr )**3.5_kr
+   real(kr),parameter :: Z = 26.0_kr
+   real(kr),parameter :: LagPref = sqrt( 1.0/720.0 ) * ( 2*Z/3.0_kr )**3.5_kr 
 
       interface
          function func(r,t,p)
@@ -53,19 +53,19 @@ module constants
       function psi1(r,t,p)
          real(kr)            :: psi1
          real(kr),intent(in) :: r,t,p
-         psi1 = sin(2*p)*sin(t)**2  * 0.25*sqrt(15.0/pi)  * LagPref*exp(-r*Z/3.0)*r**2  /sqrt(0.01483)  ! 3dxy
+         psi1 = sin(2*p)*sin(t)**2  * 0.25*sqrt(15.0/pi)  * LagPref*exp(-r*Z/3.0)*r**2  ! 3dxy
       end function psi1
 
       function psi2(r,t,p)
          real(kr)             :: psi2
          real(kr),intent(in) :: r,t,p
-         psi2 = cos(p)*sin(t)*cos(t) * sqrt(15/(4.0*pi)) * LagPref*exp(-r*Z/3.0)*r**2 / sqrt(0.01483)  ! 3dxz
+         psi2 = cos(p)*sin(t)*cos(t) * sqrt(15/(4.0*pi)) * LagPref*exp(-r*Z/3.0)*r**2 ! 3dxz
       end function psi2
 
       function psi3(r,t,p)
          real(kr)            :: psi3
          real(kr),intent(in) :: r,t,p
-         psi3 = sin(p)*sin(t)*cos(t) * sqrt(15/(4.0*pi)) * LagPref*exp(-r*Z/3.0)*r**2 / sqrt(0.01483)  ! 3dyz
+         psi3 = sin(p)*sin(t)*cos(t) * sqrt(15/(4.0*pi)) * LagPref*exp(-r*Z/3.0)*r**2  ! 3dyz
       end function psi3
 
   ! simple product of all single-particle wave functions without duplicates 3*2 = 6 functions
@@ -105,44 +105,24 @@ module constants
          Bf6 = psi3(r,t,p)*psi3(r,t,p)
       end function Bf6
 
-      function overlap(f1,f2)
-         real(kr)          :: overlap
-         real(kr),external :: f1,f2
-         integer  :: i,j,k
-         real(kr) :: r,t,p
-
-         overlap = 0.0_kr
-         do i=1,nr
-            r = (i-1)*dr
-            do j=1,ntheta
-               t = (j-1)*dtheta
-               do k=1,nphi
-                  p = (k-1)*dphi
-
-                  overlap = overlap + f1(r,t,p)*f2(r,t,p)
-
-               enddo
-            enddo
-         enddo
-         overlap = overlap*dr*dphi*dtheta
-      end function overlap
-
       subroutine overlap_mat( basisarray, nstates, omat )
          real(kr),intent(in)    :: basisarray(nstates,nr,ntheta,nphi)
          integer(ki),intent(in) :: nstates
          real(kr),intent(inout) :: omat(nstates,nstates)
          integer  :: i,j,k,m1,m2
-         real(kr) :: t
+         real(kr) :: r,t, sint
 
-         omat = 0.0_kr
+         omat = (0.0_kr)
          do m1=1,nstates
             do m2=1,nstates
                do i=1,nr
+                  r = (i-1)*dr
                   do j=1,ntheta
                      t = (j-1)*dtheta
+                     sint = sin(t)
                      do k=1,nphi
 
-                        omat(m1,m2) = omat(m1,m2) + basisarray(m1,i,j,k)*basisarray(m2,i,j,k)*sin(t)
+                        omat(m1,m2) = omat(m1,m2) + basisarray(m1,i,j,k)*basisarray(m2,i,j,k)*sint*r**2
 
                      enddo
                   enddo
@@ -196,34 +176,43 @@ module constants
 
       end subroutine diagonalize
 
-      function Utensor(m1,m2,m3,m4, nstates, basisarray )
-         real(kr)               :: Utensor
-         integer(ki),intent(in) :: m1,m2,m3,m4,nstates
-         real(kr),intent(in)    :: basisarray(nstates,nr,ntheta,nphi)
+      function Utensor_sp(m1,m2,m3,m4, basisarray )
+         real(kr)               :: Utensor_sp
+         integer(ki),intent(in) :: m1,m2,m3,m4
+         real(kr),intent(in)    :: basisarray(norb,nr,ntheta,nphi)
          integer  :: i1,j1,k1,i2,j2,k2
-         real(kr) :: r1,r2,t1,t2, dist
+         real(kr) :: r1,r2,t1,t2,p1,p2, sint1,sint2,cost1,cost2,cosp1p2, dist
 
-         Utensor = 0.0_kr
-         do i1=1,nr
-            r1 = (i1-1)*dr
-            do j1=1,ntheta
-               t1 = (j1-1)*dtheta
+         Utensor_sp = 0.0_kr
 
-               do i2=1,nr
-                  r2 = (i2-1)*dr + dr/2
-                  do j2=1,ntheta
-                     t2 = (j2-1)*dtheta + dtheta/2
+         do j1=1,ntheta
+            t1 = (j1-1)*dtheta - dtheta/10
+            sint1 = sin(t1)
+            cost1 = cos(t1)
 
-                     dist = 1.0_kr/sqrt(r1**2+r2**2-2*r1*r2*cos(t1-t2))
+            do j2=1,ntheta
+               t2 = (j2-1)*dtheta + dtheta/10
+               sint2 = sin(t2)
+               cost2 = cos(t2)
 
-                     do k1=1,nphi
-                        do k2=1,nphi
+               do k1=1,nphi
+                  p1 = (k1-1)*dphi - dphi/10
+                  do k2=1,nphi
+                     p2 = (k2-1)*dphi + dphi/10
+                     cosp1p2 = cos(p1-p2)
+
+                     do i1=1,nr
+                        r1 = (i1-1)*dr - dr/10
+                        do i2=1,nr
+                           r2 = (i2-1)*dr + dr/10
+
+                           dist = sqrt(r1*r1+r2*r2-2*r1*r2*( sint1*sint2*cosp1p2 + cost1*cost2 ) )
                      
-                           Utensor = Utensor                                           &
+                           Utensor_sp = Utensor_sp                                     &
                                 &  + basisarray(m1,i1,j1,k1)*basisarray(m2,i2,j2,k2)   &
-                                &   *dist                                              &
                                 &   *basisarray(m3,i1,j1,k1)*basisarray(m4,i2,j2,k2)   &
-                                &   *sin(t1)*sin(t2)  
+                                &   *sint1*sint2*r1*r1*r2*r2                           &
+                                &   /dist                                              
 
                         enddo
                      enddo
@@ -231,8 +220,55 @@ module constants
                enddo
             enddo
          enddo
-         Utensor = Utensor*(dr*dphi*dtheta)**2 * hartree
-      end function Utensor
+         Utensor_sp = Utensor_sp*(dr*dphi*dtheta)**2 * hartree
+      end function Utensor_sp
+
+      function Utensor_prod(m1,m2, basisarray )
+         real(kr)               :: Utensor_prod
+         integer(ki),intent(in) :: m1,m2
+         real(kr),intent(in)    :: basisarray(nprodstates,nr,ntheta,nphi)
+         integer  :: i1,j1,k1,i2,j2,k2
+         real(kr) :: r1,r2,t1,t2,p1,p2, sint1,sint2,cost1,cost2,cosp1p2, dist
+
+         Utensor_prod = 0.0_kr
+
+         do j1=1,ntheta
+            t1 = (j1-1)*dtheta - dtheta/10
+            sint1 = sin(t1)
+            cost1 = cos(t1)
+
+            do j2=1,ntheta
+               t2 = (j2-1)*dtheta + dtheta/10
+               sint2 = sin(t2)
+               cost2 = cos(t2)
+
+               do k1=1,nphi
+                  p1 = (k1-1)*dphi - dphi/10
+                  do k2=1,nphi
+                     p2 = (k2-1)*dphi + dphi/10
+                     cosp1p2 = cos(p1-p2)
+
+                     do i1=1,nr
+                        r1 = (i1-1)*dr - dr/10
+                        do i2=1,nr
+                           r2 = (i2-1)*dr + dr/10
+
+                           dist = sqrt(r1*r1+r2*r2-2*r1*r2*( sint1*sint2*cosp1p2 + cost1*cost2 ) )
+                     
+                           Utensor_prod = Utensor_prod           &
+                                &  + basisarray(m1,i1,j1,k1)     &
+                                &   *basisarray(m2,i2,j2,k2)     &
+                                &   *sint1*sint2*r1*r1*r2*r2     &
+                                &   /dist                                              
+
+                        enddo
+                     enddo
+                  enddo      
+               enddo
+            enddo
+         enddo
+         Utensor_prod = Utensor_prod*(dr*dphi*dtheta)**2 * hartree
+      end function Utensor_prod
 
 end module constants
 
@@ -298,7 +334,7 @@ program main
    call overlap_mat( spbasis, norb, omatrix_sp )
    do m1=1,norb
       do m2=1,norb
-         write(*,'(F8.5,3X)',advance='no') omatrix_sp(m1,m2)
+         write(*,'(F10.5,3X)',advance='no') omatrix_sp(m1,m2)
       enddo
       write(*,'(A)') ''
    enddo
@@ -308,7 +344,7 @@ program main
    call overlap_mat( prodbasis, nprodstates,omatrix_prod )
    do m1=1,nprodstates
       do m2=1,nprodstates
-         write(*,'(F8.5,3X)',advance='no') omatrix_prod(m1,m2)
+         write(*,'(F9.5,3X)',advance='no') omatrix_prod(m1,m2)
       enddo
       write(*,'(A)') ''
    enddo
@@ -372,41 +408,50 @@ program main
    write(*,'(A)') ''
 
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!   do m1=1,norb
-!      do m2=1,norb
-!         do m3=1,norb
-!            do m4=1,norb
-!               write(*,'(I2,I2,I2,I2,A,F8.5,3X)',advance='no') m1,m2,m3,m4,' : ',Utensor(m1,m2,m3,m4, norb,spbasis )
-!               write(*,'(A)') ''
-!            enddo
-!         enddo
-!      enddo
-!   enddo
-!   stop 0
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   write(*,'(A)') 'Calculate single-particle Umatrix...'
+   write(*,'(A)') 'Calculate single-particle U_abab matrix...'
    do m1=1,norb
       do m2=1,norb
-         write(*,'(F8.5,3X)',advance='no') Utensor(m1,m2,m1,m2, norb,spbasis )
+         write(*,'(F8.5,3X)',advance='no') Utensor_sp(m1,m2,m1,m2, spbasis )
       enddo
       write(*,'(A)') ''
    enddo
 
-   write(*,'(A)') 'Calculate single-particle Jmatrix...'
+   write(*,'(A)') 'Calculate single-particle U_abba matrix...'
    do m1=1,norb
       do m2=1,norb
-         write(*,'(F8.5,3X)',advance='no') Utensor(m1,m2,m2,m1, norb,spbasis )
+         write(*,'(F8.5,3X)',advance='no') Utensor_sp(m1,m2,m2,m1, spbasis )
       enddo
       write(*,'(A)') ''
    enddo
 
-!   write(*,'(A)') 'Calculate product basis density-density terms of the Coulomb interaction...'
-!   do m1=1,nprodstates
-!      do m2=1,nprodstates
-!         write(*,'(F8.5,3X)',advance='no') Utensor(m1,m2,m1,m2, nprodstates, prodbasis )
-!      enddo
-!      write(*,'(A)') ''
-!   enddo
+   write(*,'(A)') 'Calculate single-particle U_aabb matrix...'
+   do m1=1,norb
+      do m2=1,norb
+         write(*,'(F8.5,3X)',advance='no') Utensor_sp(m1,m1,m2,m2, spbasis )
+      enddo
+      write(*,'(A)') ''
+   enddo
+
+   write(*,'(A)') 'Full Coulomb interaction matrix in product basis...'
+   do m1=1,nprodstates
+      do m2=1,nprodstates
+         !write(*,'(F8.5,3X)',advance='no') Utensor_prod(m1,m2, prodbasis )
+    !!!!!!!!!!!!!! 
+         omatrix_prod(m1,m2) = Utensor_prod(m1,m2, prodbasis )
+         write(*,'(F8.5,3X)',advance='no') omatrix_prod(m1,m2)
+   !!!!!!!!!!!!!!!!
+      enddo
+      write(*,'(A)') ''
+   enddo
+
+   write(*,'(A)') 'Diagonalize Coulomb interaction in product basis...'
+   call diagonalize(omatrix_prod, nprodstates, evals, evecs)
+   do m1=1,nprodstates
+      write(*,'(F8.5,3X)') evals(m1)
+   enddo
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
    deallocate( prodbasis )
